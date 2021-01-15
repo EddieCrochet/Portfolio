9455bc91fb668cd63c4937a0eef1266d42f5a5be
Eddie Crochet <eddiecrochet1994@gmail.com>
2021-01-14 18:55:48 -0600

    added winning logic to the game!

--- games/Hanoi/script.js
+++ games/Hanoi/script.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const t3 = document.getElementsByClassName("t3")[0];
+
 function onDragStart(ev) {
     ev.dataTransfer.setData('text/plain', ev.target.id);
 }
@@ -13,12 +15,17 @@ function onDrop(ev) {
     const draggableEl = document.getElementById(id);
     const dropzone = ev.target;
     const blocks = ev.target.children;
-    console.log(blocks[blocks.length]);
 
+    //first 2 main rules about the id number size and if the tower was empty
     if((blocks.length === 0) || (blocks[blocks.length -1].id > id) ) {
         dropzone.prepend(draggableEl);
+        //winning logic
+        if(t3.children.length == 4) {
+            alert("dang! You won!")
+        }
+    } else {
+        alert("I already told you you cant do that...");
     }
-    console.log(id);
 
     ev.dataTransfer.clearData();
 }
