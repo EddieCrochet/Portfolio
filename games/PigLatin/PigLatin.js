a5ae428b64dc2d933a5d0c7d201389b31a01d9ae
Eddie Crochet <eddiecrochet1994@gmail.com>
2021-03-08 16:37:36 -0600

    another bug squashed! Got my 'when word starts with a y' condition all fixed and prettied up!

--- games/PigLatin/PigLatin.js
+++ games/PigLatin/PigLatin.js
@@ -10,35 +10,35 @@ let translate = (english) => {
     //code that will run to translate each individual word
     english.forEach(wrd => {
         console.log(wrd);
-        let vowels = ['a', 'e', 'i', 'o', 'u', 'y', 
-        'A', 'E', 'I', 'O', 'U', 'Y'];
         let firstPart;
         let restOfWord;
-        let vowelPos;
 
         // regex to find the vowel position is so much easier
-        vowelPos = wrd.search(/['a' 'e' 'i' 'o' 'u' 'y' 'A' 'E' 'I' 'O' 'U' 'Y']/);
+        let vowelPos = wrd.search(/['a' 'e' 'i' 'o' 'u' 'y' 'A' 'E' 'I' 'O' 'U' 'Y']/);
         console.log(vowelPos);
 
-            if(vowelPos === 0){
-                //if the first letter is a vowel
-                if(wrd[0] === 'y'){
-                    // AND if the first letter is specifically a y
-                    firstPart = wrd.substring(0, 1);
-                    restOfWord = wrd.substring(1);
-                    pigLatinArr.push(restOfWord + firstPart + "ay");
-                }
-                pigLatinArr.push(wrd+"yay");
-            }
-            else if (vowelPos === -1) {
-                //if there are no vowels
-                pigLatinArr.push(wrd+"ay");
-            }
-            else{
-                firstPart = wrd.substring(0, vowelPos);
-                restOfWord = wrd.substring(vowelPos);
-                pigLatinArr.push(restOfWord + firstPart +"ay");
-            }
+        if(wrd[0] === 'y'){
+            // if the first letter is specifically a y
+            firstPart = wrd.substring(0, 1);
+            restOfWord = wrd.substring(1);
+            vowelPos = restOfWord.search(/['a' 'e' 'i' 'o' 'u' 'y' 'A' 'E' 'I' 'O' 'U' 'Y']/);
+            firstPart = firstPart + restOfWord.substring(0, vowelPos);
+            restOfWord = restOfWord.substring(vowelPos);
+            pigLatinArr.push(restOfWord + firstPart + "ay");
+        }
+        else if(vowelPos === 0){
+            //if the first letter is a vowel
+            pigLatinArr.push(wrd+"way");
+        }
+        else if (vowelPos === -1) {
+            //if there are no vowels
+            pigLatinArr.push(wrd+"ay");
+        }
+        else{
+            firstPart = wrd.substring(0, vowelPos);
+            restOfWord = wrd.substring(vowelPos);
+            pigLatinArr.push(restOfWord + firstPart +"ay");
+        }
     });
     console.log(pigLatinArr);
 };
