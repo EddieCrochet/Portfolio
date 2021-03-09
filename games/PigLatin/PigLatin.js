bb4c6162d08abde11dfd8a1478da23cb0493e760
Eddie Crochet <eddiecrochet1994@gmail.com>
2021-03-08 20:45:28 -0600

    atatempting to remove punctuation from the beginning and end of each word

--- games/PigLatin/PigLatin.js
+++ games/PigLatin/PigLatin.js
@@ -8,15 +8,29 @@ pigLatinArr.length = 0;
 
 let translate = (english) => {
     //code that will run to translate each individual word
-    english.forEach(wrd => {
+    english.forEach(word => {
+        //only convert to lowercase, we will change first letter to capital later on
+        let wrd = word.toLowerCase();
         console.log(wrd);
         let firstPart;
         let restOfWord;
+        let lastLetter = wrd[wrd.length-1];
+        let nonLetters = [];
+        
+        // removes non letters from the END of the word and THEN runs the translation
+        if(lastLetter>'z' || lastLetter<'a'){
+            let charToRemove = wrd.slice(wrd.length-1);
+            console.log(wrd);
+            //puts non letter in designated array
+            nonLetters.push(charToRemove);
+            //actually removes non letter from original word for storage
+            wrd.replace(charToRemove, " ");
+            console.log(wrd);
+        }
 
         // regex to find the vowel position is so much easier
         let vowelPos = wrd.search(/['a' 'e' 'i' 'o' 'u' 'y' 'A' 'E' 'I' 'O' 'U' 'Y']/);
-        console.log(vowelPos);
-
+    
         if(wrd[0] === 'y'){
             // if the first letter is specifically a y
             firstPart = wrd.substring(0, 1);
@@ -24,20 +38,20 @@ let translate = (english) => {
             vowelPos = restOfWord.search(/['a' 'e' 'i' 'o' 'u' 'y' 'A' 'E' 'I' 'O' 'U' 'Y']/);
             firstPart = firstPart + restOfWord.substring(0, vowelPos);
             restOfWord = restOfWord.substring(vowelPos);
-            pigLatinArr.push(restOfWord + firstPart + "ay");
+            pigLatinArr.push(restOfWord + firstPart + "ay"+nonLetters);
         }
         else if(vowelPos === 0){
             //if the first letter is a vowel
-            pigLatinArr.push(wrd+"way");
+            pigLatinArr.push(wrd+"way"+nonLetters);
         }
         else if (vowelPos === -1) {
             //if there are no vowels
-            pigLatinArr.push(wrd+"ay");
+            pigLatinArr.push(wrd+"ay"+nonLetters);
         }
         else{
             firstPart = wrd.substring(0, vowelPos);
             restOfWord = wrd.substring(vowelPos);
-            pigLatinArr.push(restOfWord + firstPart +"ay");
+            pigLatinArr.push(restOfWord + firstPart +"ay"+nonLetters);
         }
     });
     console.log(pigLatinArr);
