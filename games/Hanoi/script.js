33d806a05d10e01aed7f48b0c3c5283b3e235145
Eddie Crochet <eddiecrochet1994@gmail.com>
2021-03-21 16:08:42 -0500

    added directions to html for clarification, also the boxes can no longer be stacked within one another AND Ihave an IsLegal function to help with seperation of concerns

--- games/Hanoi/script.js
+++ games/Hanoi/script.js
@@ -16,19 +16,31 @@ function onDragOver(ev) {
     ev.preventDefault();
 }
 
-function onDrop(ev) {
-    // Below points to the drop object's first class name!
+const isLegal = (ev) => {
+     // Below points to the drop object's first class name!
     // This is designed to be either 'tower' or 'block' to help with logic :)
     console.log(ev.path[0].classList[0]);
     const id = ev.dataTransfer.getData('text');
+    const blocks = ev.target.children;
+    //console.log(blocks[blocks.length -1].id);
+
+    // we HAVE to drop the block onto a tower...
+    if(ev.path[0].classList[0] !== "tower")
+        return false;
+        // 2 main rules about the id number size and if the tower was empty
+    if((blocks.length === 0) || (blocks[blocks.length -1].id > id))
+        return true;
+    else return false;
+};
+
+function onDrop(ev) {
+    const id = ev.dataTransfer.getData('text');
     const draggableEl = document.getElementById(id);
     const dropzone = ev.target;
-    const blocks = ev.target.children;
-
-    //first 2 main rules about the id number size and if the tower was empty
-    if((blocks.length === 0) || (blocks[blocks.length -1].id > id) ) {
+    
+    if(isLegal(ev)) {
         dropzone.prepend(draggableEl);
-        console.log(blocks[blocks.length -1].id, id);
+        console.log(id);
         //winning logic
         if(t3.children.length == 4) {
             alert("dang! You won!")
